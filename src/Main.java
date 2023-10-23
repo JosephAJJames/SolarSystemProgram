cfb09eda4c14070bb099f53934ef66eb3631e835
JosephAJJames <Joe.Centurion.7@gmail.com>
2023-10-23 17:29:41 +0100

    updated main method

--- src/Main.java
+++ src/Main.java
@@ -7,15 +7,20 @@ public class Main
         boolean play = true;
         int earthAngle = 0;
         SolarSystem mySystem = new SolarSystem(800, 800);
-        while (play) {
+        Planet sun = new Planet(0, 0, 150, "YELLOW");
+        Planet theEarth = new Planet(300, earthAngle, 35, "BLUE");
+        Moon theMoon = new Moon(300, 0, 15, "GRAY", 100, 50);
 
-            mySystem.drawSolarObject(0, 0, 150, "YELLOW");
-            mySystem.drawSolarObject(300, earthAngle, 35, "BLUE");
+        while(play) {
+            theEarth.setAngle(earthAngle);
+            mySystem.drawSolarObject((double) sun.getDistance(), (double) sun.getAngle(), (double) sun.getDiameter(), sun.getColor());
+            mySystem.drawSolarObject((double) theEarth.getDistance(), (double) theEarth.getAngle(), (double) theEarth.getDiameter(), theEarth.getColor());
+            mySystem.drawSolarObjectAbout((double) theMoon.getDistance(), (double) theMoon.getAngle(), (double) theMoon.getDiameter(), theMoon.getColor(), (double) theMoon.getRotationDistance(), (double) theMoon.getRotationAngle());
             mySystem.finishedDrawing();
-            if (!(earthAngle <= 360)) {
+            if (earthAngle > 360) {
                 earthAngle = 0;
             } else {
-                earthAngle++;
+                ++earthAngle;
             }
         }
     }
