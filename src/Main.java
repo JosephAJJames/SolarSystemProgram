3445fa7a75b7922085c856b82ca5f09193ad05da
Joseph James <Joe.Centurion.5@gmail.com>
2023-10-24 16:03:04 +0100

    Added orbital movement to the moon

--- src/Main.java
+++ src/Main.java
@@ -6,22 +6,22 @@ public class Main
     {
         boolean play = true;
         int earthAngle = 0;
+        int moonAngle = 90;
         SolarSystem mySystem = new SolarSystem(800, 800);
         Planet sun = new Planet(0, 0, 150, "YELLOW");
         Planet theEarth = new Planet(300, earthAngle, 35, "BLUE");
-        Moon theMoon = new Moon(300, 0, 15, "GRAY", 100, 50);
+        Moon theMoon = new Moon(30, moonAngle, 15, "GRAY", theEarth.getDistance(), earthAngle);
 
         while(play) {
             theEarth.setAngle(earthAngle);
+            theMoon.setAngle(moonAngle);
+            theMoon.setRotationAngle(earthAngle);
             mySystem.drawSolarObject((double) sun.getDistance(), (double) sun.getAngle(), (double) sun.getDiameter(), sun.getColor());
             mySystem.drawSolarObject((double) theEarth.getDistance(), (double) theEarth.getAngle(), (double) theEarth.getDiameter(), theEarth.getColor());
-            mySystem.drawSolarObjectAbout((double) theMoon.getDistance(), (double) theMoon.getAngle(), (double) theMoon.getDiameter(), theMoon.getColor(), (double) theMoon.getRotationDistance(), (double) theMoon.getRotationAngle());
+            mySystem.drawSolarObjectAbout(theMoon.getDistance(), theMoon.getAngle(), theMoon.getDiameter(), theMoon.getColor(), theEarth.getDistance(),theMoon.getRotationAngle());
             mySystem.finishedDrawing();
-            if (earthAngle > 360) {
-                earthAngle = 0;
-            } else {
-                ++earthAngle;
-            }
+            earthAngle = (earthAngle + 1) % 360;
+            moonAngle = (moonAngle + 5) % 360;
         }
     }
 }
